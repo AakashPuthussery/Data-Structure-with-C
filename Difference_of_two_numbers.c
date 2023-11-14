b8935eecac0fc04b2d942629d72e7fba326f0ec3
Aakash <aakashputhussery.official@gmail.com>
2023-11-14 10:48:11 +0530

    DS

---
+++ Difference_of_two_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the 1st number:\n");
+scanf("%d",&a);
+printf("Enter the 2nd number:\n");
+scanf("%d",&b);
+
+c=a-b;
+
+printf("difference between the number is:%d\n",c);
+return 0;
+}
